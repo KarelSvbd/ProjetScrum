53df5d2d09a863391666f44ec59399473691d3e6
KarelSvbd <57522089+KarelSvbd@users.noreply.github.com>
2020-05-07 09:58:35 +0200

    Update index.php

--- Site_DropShipping/Pages/index.php
+++ Site_DropShipping/Pages/index.php
@@ -10,6 +10,23 @@
         <header>
             <?php require 'header.inc.php';?>
         </header>
+        <h3>Nouvelle collection</h3>
+        
+        <div class="articleDefille"></div>
+        <div class="articleDefille"></div>
+        <div class="articleDefille"></div>
+        <div class="articleDefille"></div>
+        <div class="articleDefille"></div>
+        <div class="articleDefille"></div>
+        <div class="articleDefille"></div>
+
+        <h3>Notre Bestseller</h3>
+
+        <div class="articleBestseller"></div>
+
+        
+
+
     </body>
 </html>
 <script src=""></script>
