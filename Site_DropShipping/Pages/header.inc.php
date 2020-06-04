1a411846f29faf72572867b52b20cff9c78f0bfc
fabioRdrgs <63042066+fabioRdrgs@users.noreply.github.com>
2020-06-04 02:41:01 +0200

    df

---
+++ Site_DropShipping/Pages/header.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+
+echo "<h1>TaCite.ch</h1>";
+echo "<h2>Ta Boutique officielle</h2>";
+echo "<span style=\"font-size:30px;cursor:pointer\" onclick=\"openNav()\">&#9776; </span>";
+
+
+
+
+?>
+
+
---
+++ Site_DropShipping/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Accueil</title>
+        <link href="./CSS/Style.css" rel="stylesheet" type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <header>
+          
+        <?php require_once './pages/header.inc.php';?>
+        </header>
+         <div id="ligneRouge">.</div>
+        <h3>Nouvelle collection</h3>
+
+ <ul id="newCollection">
+        <li class="articleDefille"><img src="./Ressources/Banane.jpg" alt="Banane"></li>
+        <li class="articleDefille"><img src="./Ressources/BasketFemme.jpg" alt="Basket Femme"></li>
+        <li class="articleDefille"><img src="./Ressources/BasketHomme.jpg" alt="Basket Homme"></li>
+        <li class="articleDefille"><img src="./Ressources/Casquette.jpg" alt="Casquette"></li>
+</ul>
+        <h3>Notre Bestseller</h3>
+
+        <div class="articleBestseller"></div>  
+
+        <ul id="Bestseller">   
+           <table>
+               <tr>
+                   <td>  <li><img src="./Ressources/SurvetHomme.jpg" alt=""Survêtement Homme></li></td>
+                   <td> <li class="liBestseller"><p>Produits de qualités (certifié par nos tests)</p></li>
+            <li class="liBestseller"><p>Produits défiant peu concurance</p></li></td>
+</tr>
+</table>
+          
+           
+        </ul>
+
+
+
+        
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+
+
+
+    </body>
+</html>
+<script src="./JS/Script.js"></script>
